95d6b29e2dd0c43f55ed7d660580914b61091b35
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2023-05-18 00:26:11 -0600

    Models
    - Cración de los models para usuario y contacto.

---
+++ app/Models/ContactModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ContactModel extends Model {
+    protected $table = 'contact';
+
+    public function getAllContactsByUser(string $userId = null)
+    {
+    }
+}
