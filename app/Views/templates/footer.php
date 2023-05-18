124d67c1be4e24c6000f92e8e348dcd40f024279
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2023-05-17 23:58:59 -0600

    Templates
    - Crea templates para footer y header.

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="copyrights">
+
+        <p>&copy; <?= date('Y') ?></p>
+
+    </div>
+</footer>
+
+<!-- SCRIPTS -->
+
+<script>
+    function toggleMenu() {
+        var menuItems = document.getElementsByClassName('menu-item');
+        for (var i = 0; i < menuItems.length; i++) {
+            var menuItem = menuItems[i];
+            menuItem.classList.toggle("hidden");
+        }
+    }
+</script>
+
+<!-- -->
+
+</body>
+</html>
